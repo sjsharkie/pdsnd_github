c13241967f63a5ec147f0a9ef38005387fd56daa
sjsharkie <orinhite@gmail.com>
2019-02-22 22:47:07 -0800

    Simplify city input code

--- bikeshare.py
+++ bikeshare.py
@@ -22,8 +22,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    city = input("Let's Explore a City, Please select Chicago (ch), Washington (dc) or New York City (ny)")
-    city = city.lower()
+    city = input("Let's Explore a City, Please select Chicago (ch), Washington (dc) or New York City (ny)").lower()
     if city == 'ch':
         print("Chicago")
         city = 'chicago'
